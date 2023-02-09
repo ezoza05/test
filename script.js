f5722bf5eafda341903980a7858fd60c7ba04365
ezoza05 <101642184+ezoza05@users.noreply.github.com>
2023-02-09 23:20:47 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+     var ask = confirm('do u luv me?')
+     if( ask){
+         alert('i luv u too :3')
+     }
+     else{
+        var i = ask;
+       while(i <= 10){
+           
+      var ask1 = confirm('do u luv me?')
+    i++;
+       }
+       alert('FINE!!!')
+       alert('but i luv u<3')
+       alert(':(((')
+    }
